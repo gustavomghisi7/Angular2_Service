b4d87996652b33d70c20b400eb5f7d912974a4d1
Gustavo Martins Ghisi <guga-ghisi@hotmail.com>
2022-06-03 14:24:56 -0300

    singleton

--- src/app/cursos/cursos.service.ts
+++ src/app/cursos/cursos.service.ts
@@ -3,7 +3,18 @@ import { Injectable } from "@angular/core";
 @Injectable()
 
 export class CursosService {
+
+    private cursos: string[] = ['Angular 2', 'Java', 'Phonegap'];
+
+    constructor() {
+        console.log('CursosService');
+    }
+
     getCursos() {
-        return ['Angular 2', 'Java', 'JavaScript'];
+        return this.cursos;
+    }
+
+    addCurso(curso: string) {
+        this.cursos.push(curso);
     }
 }
