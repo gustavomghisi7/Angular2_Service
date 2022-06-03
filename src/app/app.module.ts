87fcfe0a5fb7e316409e77553d2cc1c68b0643da
Gustavo Martins Ghisi <guga-ghisi@hotmail.com>
2022-06-03 15:16:43 -0300

    serviço dentro de serviço

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CursosModule } from './cursos/cursos.module';
 import { CriarCursoModule } from './criar-curso/criar-curso.module';
+import { LogService } from './shared/log.service';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { CriarCursoModule } from './criar-curso/criar-curso.module';
     CriarCursoModule,
     CursosModule
   ],
+  providers: [ LogService ],
   //providers: [ CursosService ],
   bootstrap: [
     AppComponent
